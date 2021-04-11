c987bf74e7f7c79cbbaecf39fdd3dcc776900b09
뽕 <@DESKTOP-HA808LS>
2021-04-12 05:05:16 +0900

    4-12 로그인,회원가입 / 실패,쿠키,유지 미완

--- JHTA_PetMatching/src/main/webapp/resources/sql/member.sql
+++ JHTA_PetMatching/src/main/webapp/resources/sql/member.sql
@@ -1,11 +1,17 @@
 create table members(
-id 		 varchar2(20) primary key,
+id 		 varchar2(20),
 password varchar2(60),
 name 	 varchar2(20),
 phone 	 varchar2(60),
 gender 	 varchar2(20),
-position varchar2(20)
+position varchar2(20),
+auth	 varchar2(30) not null,
+PRIMARY KEY(id)
 );
+
+update members
+set auth = 'SET_ADMIN'
+where id = 'admin';
 
 select *
 from members
