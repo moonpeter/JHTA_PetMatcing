ffc7161ccf82f5101c596b9a3a039db9bda9f1c1
moonpeter <moonpeter@kakao.com>
2021-04-14 11:07:08 +0900

    [4/14] 쇼핑몰 - 파일 업로드 관련 작업 중(난수 + 날짜로 폴더 생성하기)

--- JHTA_PetMatching/src/main/java/com/jhta/petMatching/mkh/controller/ShopController.java
+++ JHTA_PetMatching/src/main/java/com/jhta/petMatching/mkh/controller/ShopController.java
@@ -1,10 +1,13 @@
 package com.jhta.petMatching.mkh.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +27,9 @@ public class ShopController {
 	@Autowired
 	ShopService shopService;
 	
+	@Value("${savefoldername_shop")
+	private String saveFolder;
+	
 	@GetMapping("main")
 	public String mainPage() {
 		return "shop/shop_main";
@@ -40,12 +46,53 @@ public class ShopController {
 	}
 	
 	@PostMapping("write")
-	public String insert(ShopBoard shopBoard) {
-//		MultipartFile uploadFile = shopBoard.getUploadFile();
+	public String insert(ShopBoard shopBoard) throws Exception {
+		MultipartFile uploadThumnail = shopBoard.getShop_upload_thumnail();
+		MultipartFile uploadImgContent = shopBoard.getShop_upload_img_content();
+		
+		if (!uploadThumnail.isEmpty()) {
+			String fileName = uploadThumnail.getOriginalFilename(); // 썸네일의 원래 파일명 
+			shopBoard.setShop_thumnail_original(fileName); // 썸네일의 원래 파일명 저장
+			String fileDBName = fileDBName(fileName, saveFolder);
+			logger.info("fileDBName = " + fileDBName);
+			
+			uploadThumnail.transferTo(new File(saveFolder + fileDBName));
+			
+			shopBoard.setShop_thumnail(fileDBName);
+		}
+		
+		if (!uploadImgContent.isEmpty()) {
+			String fileName = uploadImgContent.getOriginalFilename(); // 썸네일의 원래 파일명 
+			shopBoard.setShop_img_content_original(fileName); // 썸네일의 원래 파일명 저장
+			String fileDBName = fileDBName(fileName, saveFolder);
+			logger.info("fileDBName = " + fileDBName);
+			
+			uploadImgContent.transferTo(new File(saveFolder + fileDBName));
+			
+			shopBoard.setShop_img_content(fileDBName);
+		}
 		
 		shopService.insertShopBoard(shopBoard);
 		
 		return "redirect:main";
+	}
+
+	private String fileDBName(String fileName, String saveFolder) {
+		
+		// 새로운 폴더 이름 : 오늘 + 년 + 월 + 일 
+		Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH) + 1;
+		int date = c.get(Calendar.DATE);
+		
+		String homedir = saveFolder + year + "-" + "month" + "date";
+		logger.info(homedir);
+		File path1 = new File(homedir);
+		if(!(path1.exists())) {	// 
+			path1.mkdir();		// 새로운 폴더를 생성 
+		}
+		
+		return null;
 	}
 	
 }
--- JHTA_PetMatching/src/main/webapp/resources/sql/shop/shop.sql
+++ JHTA_PetMatching/src/main/webapp/resources/sql/shop/shop.sql
@@ -7,9 +7,14 @@ create table shop_board(
 	shop_price					varchar2(10) not null,		-- 가격
 	shop_country_of_origin		varchar2(10) not null,		-- 원산지
 	shop_brand					varchar2(20) not null,		-- 브랜드
+	
 	shop_thumnail				varchar2(50) not null,		-- 썸네일
+	shop_thumnail_original		varchar2(50) not null,
+	
 	shop_grade					varchar2(10), 				-- 평점
 	shop_img_content			varchar2(50),				-- 본문 이미지 파일
+	shop_img_content_original	varchar2(50),
+	
 	shop_text_content			varchar2(3000)				-- 본문 텍스트
 );
 
