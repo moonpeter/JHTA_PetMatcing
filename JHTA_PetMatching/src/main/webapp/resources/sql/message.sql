69f35cf35e0c23ce5f08e53c66994716d3df8093
moonpeter <57426244+moonpeter@users.noreply.github.com>
2021-04-26 14:26:55 +0900

    Update message.sql

--- JHTA_PetMatching/src/main/webapp/resources/sql/message.sql
+++ JHTA_PetMatching/src/main/webapp/resources/sql/message.sql
@@ -14,16 +14,16 @@ cancelby_sender    varchar2(1) default 'n' check(cancelby_sender in ('n', 'y')),
 read_count         number default 0, 
 register_date      date default sysdate  
 );
---read_count ����
---���� �޽����Կ��� �޽��� �󼼺����, message_num�� select�Ͽ� �ش� �޽����� read_count�� +1 ��½�Ų��. 
---�̰��� �̿��Ͽ� ���� �޽������� new message���� ���� �� �ְ�
---������ ���� �޽����Կ��� ���� �޽����� �������� ���������� Ȯ���� �� �ִ�. 
---���� �޽����Կ��� �޽��� ��ȸ�� ������ ��ȸ���� ������� �ʴ´�.
+--read_count 설명
+--받은 메시지함에서 메시지 상세보기시, message_num로 select하여 해당 메시지의 read_count를 +1 상승시킨다. 
+--이것을 이용하여 받은 메시지함의 new message수를 구할 수 있고
+--상대방이 보낸 메시지함에서 보낸 메시지가 읽혔는지 안읽혔는지 확인할 수 있다. 
+--보낸 메시지함에서 메시지 조회할 때에는 조회수가 상승하지 않는다.
 
 
---������ �ۼ�
-create sequence message_seq;
 
-
---������ ����
+--시퀀스 삭제
 drop sequence message_seq;
+
+--시퀀스 작성
+create sequence message_seq;
